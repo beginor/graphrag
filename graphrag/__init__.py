a8ce52d0cb046bca2fce88231d82586e3e772c1e
beginor <beginor@qq.com>
2025-02-13 08:29:00 +0000

    fix model encoding issues

--- graphrag/__init__.py
+++ graphrag/__init__.py
@@ -2,3 +2,8 @@
 # Licensed under the MIT License
 
 """The GraphRAG package."""
+import tiktoken
+
+
+tiktoken.model.MODEL_TO_ENCODING['qwen2.5:14b-instruct-q8_0'] = 'cl100k_base'
+tiktoken.model.MODEL_TO_ENCODING['bge-m3:latest'] = 'cl100k_base'
