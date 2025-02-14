4a46bd85a97925d6b8954cf4a7099bfb948d195f
beginor <beginor@qq.com>
2025-02-14 11:15:25 +0800

    update test

--- webapi/test.py
+++ webapi/test.py
@@ -8,13 +8,13 @@ import asyncio
 from .search_helper import do_global_search, do_local_search
 
 
-root = 'ragtest/grimm-stories'
+root = 'ragtest/laws'
 
 async def test_global_search():
 
     async for result in do_global_search(
         root=root,
-        query='渔夫和金鱼的故事说明了什么？'
+        query='中华人民共和国大气污染防治法概述'
     ):
         print(result, end='')
 
@@ -23,10 +23,10 @@ async def test_local_search():
 
     async for result in do_local_search(
         root=root,
-        query='比目鱼直接实现了渔夫的妻子许下的哪些愿望，请给出一个列表？'
+        query='什么是水资源？'
     ):
         print(result, end='')
 
 
 if __name__ == '__main__':
-    asyncio.run(test_local_search())
+    asyncio.run(test_global_search())
